4dbea77bf0b811310211b7b9985c289233f29f0e
Jean <Jeanclenor2060@gmail.com>
2025-08-02 15:19:51 +0200

    Moving files

--- todo.js
+++ todo.js
@@ -7,43 +7,77 @@ const rl = readline.createInterface({
 
 let introduction = {
   introduction: "Welcome to the CLI project",
-  instruction: "You can only add,delete, and edit tasks",
-  addTask: "Add Task(Choose 1)",
-  deleteTask: "Add Task(Choose 2 also you start from 0-3 to delete)",
-  editTask: "Add Task(Choose 3)",
-  displayTask: "Add Task(Choose 4)",
-  finalMessage: "Goodbye!",
+  instruction: "You can only add,delete,display, and edit tasks",
 };
-
-console.log(introduction.introduction);
-console.log(introduction.instruction);
-console.log(introduction.finalMessage);
 
 let list = [];
 
-function addTask(a) {
+function menu() {
+  console.log(introduction.introduction);
+  console.log(introduction.instruction);
+
+  function userSelection() {
+    rl.question("Select an Option: Add,Delete,Edit", (input) => {
+      if (input.trim().toLowerCase() === "add") {
+        addTask();
+      } else if (input.trim().toLowerCase() === "delete") {
+        deleteTask();
+      } else if (input.trim().toLowerCase() === "edit") {
+        editTask();
+      } else if (input.trim().toLowerCase() === "display") {
+        displayTasks();
+      } else if (input.trim().toLowerCase() === "exit") {
+        rl.close();
+      } else {
+        console.log("Make up your mind!");
+        userSelection();
+      }
+    });
+  }
+  userSelection();
+}
+
+function addTask() {
   rl.question("Enter a task: ", (input) => {
     console.log(`You entered ${input}`);
     list.push(input);
-    rl.close();
-  });
-}
-addTask(list);
-
-// Going to allow user to delete task here
-
-function deleteTask(b) {
-  rl.question("Delete a task: ", (input) => {
-    console.log(`You deleted ${input}`);
-
-    const itemToRemove = input;
-
-    const newList = list.filter((item) => item !== itemToRemove);
-    console.log(newList);
-    rl.close();
+    console.log(list);
+    menu();
   });
 }
 
-deleteTask(list);
+function deleteTask() {
+  rl.question("Select a task that you want to delete: ", (input) => {
+    console.log(list);
+    let elementToRemove = input;
+    let indexToRemove = list.indexOf(elementToRemove);
+    if (indexToRemove !== -1) {
+      list.splice(indexToRemove, 1);
+    }
+    console.log(list);
+    menu();
+  });
+}
 
-// Editing goes down here
+function editTask() {
+  rl.question("Select an index: ", (indexInput) => {
+    let index = parseInt(indexInput, 10) - 1;
+    if (index >= 0 && index < list.length) {
+      rl.question("Enter the new value: ", (newValue) => {
+        list[index] = newValue;
+        console.log(list);
+        menu();
+      });
+    } else {
+      console.log("Invalid index.");
+      menu();
+    }
+  });
+}
+
+function displayTasks() {
+  console.log(list);
+  menu();
+}
+
+menu();
